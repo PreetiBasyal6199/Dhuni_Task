3f40036d374476d2a0f414e6efe6079909faa409
Preeti Basyal <preetibasyal412@gmail.com>
2022-12-22 23:40:19 +0545

    Unique Constraints added for tables

--- data/models.py
+++ data/models.py
@@ -3,15 +3,30 @@ from django.db import models
 # Create your models here.
 
 class Job(models.Model):
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128, unique=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.title=self.title.lower()
+        super().save(*args,**kwargs)
 
 
 class JobSkill(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     skill = models.CharField(max_length=128)
 
+    class Meta:
+        unique_together=("job_id", "skill")
+
     def __str__(self) -> str:
         return f"{self.job.title} {self.skill}"
+
+    def save(self, *args, **kwargs):
+        self.skill= self.skill.lower()
+        super().save(*args,**kwargs)
+
 
 
 class Candidate(models.Model):
@@ -30,5 +45,12 @@ class CandidateSkill(models.Model):
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
     skill = models.CharField(max_length=128)
 
+    class Meta:
+        unique_together=("candidate_id", "skill")
+
     def __str__(self) -> str:
         return self.candidate.get_full_name
+
+    def save(self, *args, **kwargs):
+        self.skill=self.skill.lower()
+        super().save(*args,**kwargs)

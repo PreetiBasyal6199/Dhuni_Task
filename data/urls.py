f0a1f4926cd09472b892012b19e7ffc9c5bd3ec0
Preeti Basyal <preetibasyal412@gmail.com>
2022-12-23 12:06:42 +0545

    New Urls added

--- data/urls.py
+++ data/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .api import MostMatchedCandidateListView
+from .api import (MostMatchedCandidateListView, JobListCreateView, JobSkillListCreateView, CandidateListCreateView, CandidateSkillListCreateView)
 
 
 urlpatterns = [
-    path('most-matched/',MostMatchedCandidateListView.as_view(), name='most_matched_candidateview'),
+    path('job/',JobListCreateView.as_view(), name='job_list_create_view'),
+    path('job-skill/',JobSkillListCreateView.as_view(), name='job_skill_list_create'),
+    path('candidate/',CandidateListCreateView.as_view(), name='candidate_list_create'),
+    path('candidate-skill/',CandidateSkillListCreateView.as_view(), name='candidate_skill_list_create'),
+    path('candidates/most-matched/',MostMatchedCandidateListView.as_view(), name='most_matched_candidateview'),
 ]
